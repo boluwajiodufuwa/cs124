5fa3dd1972b011d580f7dada119940eb0c81877c
Boluwaji <bodufuwa@college.harvard.edu>
2023-03-23 20:38:38 -0400

    ascii reader function and more helper functions

--- P2/strassen.cpp
+++ P2/strassen.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 /*
-Note that Matrices are represented in the following form "int matrix[width*height];"
+Note that Matrices are represented in the following form "int *matrix = new int[width*height];"
 where each value in the matrix is an int. See this link for more info:
 https://stackoverflow.com/questions/256297/best-way-to-represent-a-2-d-array-in-c-with-size-determined-at-run-time
 
@@ -18,6 +18,7 @@ For example, the array with dimension 2 {0,1,2,3} appears like the following:
 0 2
 1 3
 */
+
 
 // ----------------------------------------------------------------
 // Matrix Helper functions
@@ -51,6 +52,15 @@ void getRandomMatrix(int dimension, int *result) {
 
 }
 
+void printMatrix(int *matrix, int dim) {
+    for (int r = 0; r < dim; r++) {
+        for (int c = 0; c < dim; c++) {
+            cout << getMatrixValue(matrix, r, c, dim) << " ";
+        }
+        cout << endl;
+    }
+}
+
 // ----------------------------------------------------------------
 // Matrix Multiplication functions
 // ----------------------------------------------------------------
@@ -69,13 +79,68 @@ void conventional(int dimension, int *ma, int *mb, int *result) {
     }
 }
 
+
+// ----------------------------------------------------------------
+// Miscellaneous functions
+// ----------------------------------------------------------------
+
+// Reads an Ascii file with 2d^2 lines and creates two d by d matrices
+pair<int*, int*> asciiToMatrices(char* fn, int dim) {
+    // Open the file using the filename from the command line argument
+    ifstream file(fn);
+
+    // Check that the file was opened successfully
+    if (!file.is_open()) {
+        cerr << "Error: could not open file " << fn << "\n";
+    }
+
+    // Create matrices
+    int *ma = new int[dim*dim];
+    int *mb = new int[dim*dim];
+
+    // Read content of files into matrices
+    string line;
+    int counter = 0;
+    while (getline(file, line)) {
+        int val = stoi(line);
+        int col = counter % dim;
+        int row = (counter - col) / dim;
+        if (counter < dim*dim) {
+            setMatrixValue(ma, row, col, dim, val);
+        } else {
+            setMatrixValue(mb, row - dim, col, dim, val);
+        }
+        counter++;
+    }
+
+    // Close the file
+    file.close();
+
+    return make_pair(ma,mb);
+}
+
+// Reads an Ascii file with 2d^2 lines and returns the product of the resulting matrices
+void asciiToProduct(string fn, int dim, int *result) {
+
+}
+
+// ----------------------------------------------------------------
+// Main
+// ----------------------------------------------------------------
+
 int main(int argc, char *argv[]) {
     int do_tests = atoi(argv[1]);
     int dimension = atoi(argv[2]);
-    int inputfile = atoi(argv[3]);
+    char* inputfile = argv[3];
 
     if (do_tests == 1) {
         run_tests();
+    }
+
+    if (inputfile[0] != '\0') {
+        printMatrix(asciiToMatrices(inputfile, dimension).first, dimension);
+        cout << "\n\n";
+        printMatrix(asciiToMatrices(inputfile, dimension).second, dimension);
     }
 
     return 0;
