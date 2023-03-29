e5a48025a9cb864e17a0fdff34ab91c6739aeb9d
githenael2 <90978770+githenael2@users.noreply.github.com>
2023-03-29 16:30:29 -0400

    Update tests.cpp

--- P2/tests.cpp
+++ P2/tests.cpp
@@ -50,11 +50,51 @@ bool test_conventional() {
     delete[] result;
 }
 
+bool test_strassen() {
+    int dim = 2;
+    int *ma = new int[dim*dim];
+    instantiateMatrix(ma, (int[]) {0,1,2,3}, dim);
+    int *mb = new int[dim*dim];
+    instantiateMatrix(mb, (int[]) {4,5,6,7}, dim);
+    int *test_solution = new int[dim*dim];
+    instantiateMatrix(test_solution, (int[]) {10,19,14,27}, dim);
+    int *result = new int[dim*dim];
+
+    strassen(dim, ma, mb, result);
+
+    if (!compareMatrices(result, test_solution, dim)) {
+        cout << result[0] << endl;
+        cout << test_solution[0] << endl;  
+        function_failed("test_strassen", __LINE__);
+        return false;
+    }
+
+    instantiateMatrix(ma, (int[]) {0,6,2,200}, dim);
+    instantiateMatrix(mb, (int[]) {4,9,6,-8}, dim);
+    instantiateMatrix(test_solution, (int[]) {18,1824,-16,-1564}, dim);
+
+    strassen(dim, ma, mb, result);
+
+    if (!compareMatrices(result, test_solution, dim)) {
+        cout << result[1] << endl;
+        cout << test_solution[1] << endl;  
+        function_failed("test_strassen", __LINE__);
+        return false;
+    }
+    
+    return true;
+    delete[] ma;
+    delete[] mb;
+    delete[] test_solution;
+    delete[] result;
+}
+
 int run_tests() {
     cout << "Running tests\n" << endl;
 
     bool all_tests_passed = true;
     all_tests_passed = all_tests_passed && test_conventional();
+    all_tests_passed = all_tests_passed && test_strassen();
 
     if (all_tests_passed) {
         cout << "All tests have been passed successfully!\n" << endl;
