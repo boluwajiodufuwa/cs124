5232f8ba6d983fb83ff2d3f97c97788536c7fda8
githenael2 <90978770+githenael2@users.noreply.github.com>
2023-03-30 02:51:20 -0400

    Update tests.cpp

--- P2/tests.cpp
+++ P2/tests.cpp
@@ -85,7 +85,7 @@ bool test_strassen() {
     int *test_solution = new int[dim*dim];
     instantiateMatrix(test_solution, (int[]) {10,19,14,27}, dim);
     int *result = new int[dim*dim];
-
+    
     strassen(dim, ma, mb, result, 1);
 
     if (!compareMatrices(result, test_solution, dim)) {
@@ -98,12 +98,47 @@ bool test_strassen() {
     instantiateMatrix(ma, (int[]) {0,6,2,200}, dim);
     instantiateMatrix(mb, (int[]) {4,9,6,-8}, dim);
     instantiateMatrix(test_solution, (int[]) {18,1824,-16,-1564}, dim);
-
-    strassen(dim, ma, mb, result,1);
+    strassen(dim, ma, mb, result, 1);
 
     if (!compareMatrices(result, test_solution, dim)) {
         cout << result[1] << endl;
         cout << test_solution[1] << endl;  
+        function_failed("test_strassen", __LINE__);
+        return false;
+    }
+    
+    int dim_2 = 3;
+    int *odd_ma = new int[dim_2*dim_2];
+    int *odd_mb = new int[dim_2*dim_2];
+    int *odd_test_solution = new int[dim_2*dim_2];
+    int *odd_result = new int[dim_2*dim_2];
+    instantiateMatrix(odd_ma, (int[]) {1,2,3,4,5,6,7,8,9}, dim_2);
+    instantiateMatrix(odd_mb, (int[]) {2,2,2,2,2,2,2,2,2}, dim_2);
+    instantiateMatrix(odd_test_solution, (int[]) {24,30,36,24,30,36,24,30,36}, dim_2);
+    strassen(dim_2, odd_ma, odd_mb, odd_result, 1);
+
+    if (!compareMatrices(odd_result, odd_test_solution, dim_2)) {
+        cout << odd_result[1] << endl;
+        cout << odd_test_solution[1] << endl;  
+        function_failed("test_strassen", __LINE__);
+        return false;
+    }
+
+    int even_dim = 4;
+    int *ma2 = new int[even_dim*even_dim];
+    int *mb2 = new int[even_dim*even_dim];
+    int *result2 = new int[even_dim*even_dim];
+    int *test_solution2 = new int[even_dim*even_dim];
+    instantiateMatrix(ma2, (int[]) {3,4,0,2,3,4,31,2,9,-1,22,3,4,1,2,3}, even_dim);
+    instantiateMatrix(mb2, (int[]) {8,3,4,2,9,22,3,-33,6,11,1,900,4,1,2,3}, even_dim);
+    instantiateMatrix(test_solution2, (int[]) {77,42,185,40,-12,88,682,-28,3660,967,2163,2737,45,21,81,25}, even_dim);
+
+    strassen(even_dim, ma2, mb2, result2,1);
+
+    if (!compareMatrices(result2, test_solution2, even_dim)) {
+        printMatrix(ma2, even_dim);
+        printMatrix(mb, even_dim);
+        printMatrix(result, even_dim);
         function_failed("test_strassen", __LINE__);
         return false;
     }
@@ -112,32 +147,14 @@ bool test_strassen() {
     delete[] mb;
     delete[] test_solution;
     delete[] result;
-
-    dim = 4;
-    int *ma2 = new int[dim*dim];
-    int *mb2 = new int[dim*dim];
-    int *result2 = new int[dim*dim];
-    int *test_solution2 = new int[dim*dim];
-    instantiateMatrix(ma2, (int[]) {3,4,0,2,3,4,31,2,9,-1,22,3,4,1,2,3}, dim);
-    instantiateMatrix(mb2, (int[]) {8,3,4,2,9,22,3,-33,6,11,1,900,4,1,2,3}, dim);
-    instantiateMatrix(test_solution2, (int[]) {77,42,185,40,-12,88,682,-28,3660,967,2163,2737,45,21,81,25}, dim);
-
-    strassen(dim, ma2, mb2, result2,1);
-
-    if (!compareMatrices(result2, test_solution2, dim)) {
-        printMatrix(ma2, dim);
-        printMatrix(mb, dim);
-        printMatrix(result, dim);
-
-        function_failed("test_strassen", __LINE__);
-        return false;
-    }
-
+    delete[] odd_ma;
+    delete[] odd_mb;
+    delete[] odd_test_solution;
+    delete[] odd_result;
     delete[] ma2;
     delete[] mb2;
     delete[] test_solution2;
     delete[] result2;
-
     return true;
 }
 
