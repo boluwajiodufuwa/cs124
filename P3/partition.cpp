9b996cdbe4b091041761ac9f2eba7423bb48f97e
Boluwaji <bodufuwa@college.harvard.edu>
2023-04-17 21:18:53 -0400

    boiler plate for P3

---
+++ P3/partition.cpp
@@ -0,0 +1,40 @@
+#include <chrono>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Note that solutions of any form and inputs/sequences are just represented by regular vectors
+
+// ----------------------------------------------------------------
+// Miscellaneous functions
+// ----------------------------------------------------------------
+
+// Takes in a solution in the standard form (1s and -1s) and a sequence and returns residue
+vector<int> residue(vector<int> solution, vector<int> seq) {
+    int resiude = 0;
+    for (int i = 0; i < solution.size(); i++) {
+        residue += solution[i] * seq[i];
+    }
+    return residue;
+}
+
+// ----------------------------------------------------------------
+// Heuristic Functions
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+// Main
+// ----------------------------------------------------------------
+
+int main(int argc, char *argv[]) {
+    int do_tests = atoi(argv[1]);
+    int algorithm = atoi(argv[2]);
+    char* inputfile = argv[3];
+
+    return 0;
+}
